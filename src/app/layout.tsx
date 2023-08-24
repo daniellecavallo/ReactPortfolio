1fcf81876d3dedb8a5858dad3ef9a43c8179d5ef
University <university@AA687692-Mac.local>
2023-08-23 23:28:44 -0500

    updated description under title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,11 @@ export const metadata: Metadata = {
       'The Cavallo Company- Marketing, Sales, web development, and philanthropy',
   },
   description:
-    'I’m Spencer, a software designer and entrepreneur based in New York City. I’m the founder and CEO of Planetaria, where we develop technologies that empower regular people to explore space on their own terms.',
+   'At The Cavallo Company, we take pride in being a comprehensive partner for all your marketing and branding needs. As the owner of the company, I specialize in crafting 360-degree marketing and advertising plans that leave a lasting impact on your target audience. Our expertise extends beyond strategizing; we also possess the technical prowess to build cutting-edge websites and apps that align seamlessly with your brand identity.',
+   description:
+   'With a passion for innovation and a commitment to excellence, we work hand in hand with businesses to elevate their brand presence and achieve their goals in the digital landscape. Whether you seek a dynamic marketing strategy, a captivating website, or a user-friendly app, The Cavallo Company is your go-to solution for building a robust and cohesive brand experience.',
+   description:
+   'Join us in the journey of transforming your vision into reality and unlocking the full potential of your brand. Connect with us for personalized, results-driven solutions that drive growth and success.',
   alternates: {
     types: {
       'application/rss+xml': `${process.env.NEXT_PUBLIC_SITE_URL}/feed.xml`,
