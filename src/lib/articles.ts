4841c9b61b52ce4059eaf2009fc0b958619bb477
Puentebravo <puentematos@gmail.com>
2023-08-30 19:56:43 -0400

    updated path for article slug

--- src/lib/articles.ts
+++ src/lib/articles.ts
@@ -14,7 +14,7 @@ export interface ArticleWithSlug extends Article {
 async function importArticle(
   articleFilename: string,
 ): Promise<ArticleWithSlug> {
-  let { article } = (await import(`../app/articles/${articleFilename}`)) as {
+  let { article } = (await import(`../app/services/${articleFilename}`)) as {
     default: React.ComponentType
     article: Article
   }
