f0e2fd39cda10eddce539e07c880bc86ef6f461a
Puentebravo <puentematos@gmail.com>
2023-08-30 21:16:26 -0400

    Fixed pathing for article load functions

--- src/lib/articles.ts
+++ src/lib/articles.ts
@@ -14,7 +14,7 @@ export interface ArticleWithSlug extends Article {
 async function importArticle(
   articleFilename: string,
 ): Promise<ArticleWithSlug> {
-  let { article } = (await import(`../app/services/${articleFilename}`)) as {
+  let { article } = (await import(`../app/demo/${articleFilename}`)) as {
     default: React.ComponentType
     article: Article
   }
@@ -27,7 +27,7 @@ async function importArticle(
 
 export async function getAllArticles() {
   let articleFilenames = await glob('*/page.mdx', {
-    cwd: './src/app/articles',
+    cwd: './src/app/demo',
   })
 
   let articles = await Promise.all(articleFilenames.map(importArticle))
